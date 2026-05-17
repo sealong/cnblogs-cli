aad72fbdc383206723417808f7845fc6dce6d20e
sealong <353926+sealong@users.noreply.github.com>
2026-05-17 15:54:14 +0800

    feat(api): add get_news_detail function for news detail

--- src/api/news.rs
+++ src/api/news.rs
@@ -20,3 +20,16 @@ pub async fn raw_list_news(c: &Client, page: impl Serialize + Send + Sync) -> Re
     .await
     .into_anyhow_result()
 }
+
+pub async fn get_news_detail(c: &Client, id: u64) -> Result<String> {
+    let resp = raw_get_news_detail(c, id).await?;
+    resp.error_for_status()?
+        .text()
+        .await
+        .into_anyhow_result()
+}
+
+pub async fn raw_get_news_detail(c: &Client, id: u64) -> Result<Response> {
+    let url = format!("{}/newsitems/{}", OPENAPI, id);
+    c.get(url).send().await.into_anyhow_result()
+}
